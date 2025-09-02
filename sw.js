38b338d8c918320682723f8e3602a128b40b966f
shary-2000 <Bboyshary.2000@outlook.fr>
2025-09-02 11:48:31 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,24 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('v1').then(cache => cache.addAll([
-      '/',
-      '/index.html',
-      '/manifest.json',
-      '/icon-192.png',
-      '/icon-512.png'
-    ]))
+    caches.open('v1').then(cache => 
+      cache.addAll([
+        '/',                  // page principale
+        '/index.html',        // ton HTML
+        '/manifest.json',     // manifest PWA
+        '/icon-192.png',      // icônes
+        '/icon-512.png',
+        '/style.css',         // ton CSS externe si tu en utilises un
+        '/app.js'             // ton JS externe si tu en utilises un
+      ])
+    )
   );
 });
 
+// Intercepter les requêtes pour servir le cache si disponible
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
+
