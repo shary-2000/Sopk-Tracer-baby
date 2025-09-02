5e1580c7358fea2f8ba6b83be7e656acfd6bdf6e
shary-2000 <Bboyshary.2000@outlook.fr>
2025-09-02 12:04:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,10 +9,10 @@ self.addEventListener('install', event => {
     ]))
   );
 });
-
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
+
 
