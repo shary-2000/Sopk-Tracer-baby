8ae9cfcbff23d6f968d6a231f4a6aa693b36bf0d
shary-2000 <Bboyshary.2000@outlook.fr>
2025-09-02 12:11:35 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,17 @@
-const CACHE_NAME = 'v2'; // version du cache
-const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png',
-  '/style.css',   // CSS externe
-  '/app.js'       // JS externe
-];
-
-// Installer le service worker et mettre en cache les fichiers
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open('v1').then(cache => cache.addAll([
+      './',
+      './index.html',
+      './manifest.json',
+      './icon-192.png',
+      './icon-512.png'
+    ]))
   );
 });
 
-// Activer le service worker et supprimer les anciens caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      )
-    )
-  );
-});
-
-// Intercepter les requêtes pour servir le cache si disponible
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
